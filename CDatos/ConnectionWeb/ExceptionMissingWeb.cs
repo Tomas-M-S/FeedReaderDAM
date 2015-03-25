ec8a929d9eec3f3da015c95ec40f3f79885a2a3a
Tomas Martínez Sempere <tomas.radagast@gmail.com>
2015-03-25 23:19:36 +0100

    Añadida BD y diversas clases relacionadas con la capa de datos

---
+++ CDatos/ConnectionWeb/ExceptionMissingWeb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CDatos.ConnectionWeb
+{
+    public class ExceptionMissingWeb : Exception
+    {
+        public ExceptionMissingWeb() {}
+
+        public ExceptionMissingWeb(string message) : base(message) {}
+
+        public ExceptionMissingWeb(string message, Exception inner) : base(message, inner) {}
+    }
+}
---
+++ CDatos/ConnectionWeb/GetDataWeb.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDatos.ConnectionWeb
+{
+    public class GetDataWeb
+    {
+        protected String feedURI;
+        protected XDocument XMLDoc;
+
+        public GetDataWeb(String feedURI)
+        {
+            this.feedURI = feedURI;
+            this.XMLDoc = new XDocument();
+        }
+
+        public XDocument getStringFeed()
+        {
+            try
+            {
+                this.XMLDoc = XDocument.Load(feedURI);
+                return this.XMLDoc;
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                throw new ExceptionMissingWeb("Probablemente el rss no está en línea. " + Environment.NewLine + ex.Message, ex);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                throw new Exception("Error desconocido: " + ex.Message, ex);
+            }
+        }
+    }
+}
