0bdb00003bbc3074af43d023e6d86c876b29c0fe
Tomas Martínez Sempere <tomas.radagast@gmail.com>
2015-04-24 23:06:21 +0200

    New EventArg class (I forget upload)

---
+++ CNegocio/WBManager/UpdatedEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CDatos.DBEntities;
+
+namespace CNegocio.WBManager
+{
+    public class UpdatedEventArgs : EventArgs
+    {
+        public int status { get; set; }
+        public RSSContactDB rss { get; set; }
+        public DateTime time1 { get; set; }
+        public DateTime time2 { get; set; }
+    }
+}
